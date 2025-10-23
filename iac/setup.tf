65c3ec02999e501d887236ecddd4fcf9ebecc2d0
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-10-23 22:45:53 +0500

    Variables and setup terraform files added.

---
+++ iac/setup.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0b3fc46967f51ea31425efe30508c15ea05129017fe7cf22816e8b5b378e0bc3
+size 207
---
+++ iac/vars.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b46dede46dd378d13a1aa3315b1bc146ce4bf60badc91f056199c00ac087f76d
+size 429
