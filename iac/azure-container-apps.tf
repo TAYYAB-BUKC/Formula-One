349e367601eba3c6e4f2703d878022096a70431d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-10-26 08:24:30 +0500

    Naming convention for container applied

--- iac/azure-container-apps.tf
+++ iac/azure-container-apps.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8428c7b2b847953887f90e53f876908a8b44264f7915cdd0ba71543e748fee65
+oid sha256:feb6019d959731693f117bb221b7eaa93fd13aed0b3463564cf511e6f016e9ca
 size 882
