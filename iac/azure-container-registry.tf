25f983e063efaa0881b68d91541f528596e6686b
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-10-26 08:49:56 +0500

    Naming convention for container registry applied.

--- iac/azure-container-registry.tf
+++ iac/azure-container-registry.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7de491900ca6356f30f6bc69315f24b5b69930f5cf3140565373e8177e793710
-size 448
+oid sha256:0bd545d4cbd7b2ee65281d4304c040d23ba1c88ec79df4b53d288ba1481f74aa
+size 444
