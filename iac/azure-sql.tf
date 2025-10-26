f4f4e185cba76a28858c3b6e4076a2f7ced90b6d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-10-26 09:10:56 +0500

    Downgrading the SKU of SQL database to S0.

--- iac/azure-sql.tf
+++ iac/azure-sql.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a2b84b5341ec106efab0871934c12d73cfb7ab9b3d673706ed2d4ac169c2369
+oid sha256:7c939c4f4f0dab5f115886ae52969ac3a89373863f4d2233e0c49d4737b9ea19
 size 1319
