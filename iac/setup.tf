ea79b042b736fb97c791e566b4704ed76f50aae9
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-10-27 22:45:50 +0500

    Resource values added.

--- iac/setup.tf
+++ iac/setup.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32e6ddd6bf7b784757ead2926169d24a225f3423dbf4601df02707bec2a8a313
-size 417
+oid sha256:5fd9794a00db4c16451b6bab954f1b707ce5ff267304e9b2cab62fba1c00b211
+size 409
